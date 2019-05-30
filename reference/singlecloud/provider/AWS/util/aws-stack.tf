33c2586535114afaf90d8b8f7376169414126cd0
arunk4m <arunk4m@gmail.com>
2019-05-30 14:05:37 +0530

    modified code

--- reference/singlecloud/provider/AWS/util/aws-stack.tf
+++ reference/singlecloud/provider/AWS/util/aws-stack.tf
@@ -1,10 +1,10 @@
 module "aws_infra" {
-  source = "github.com/arunk4m/vThunder-terraform/tree/master/modules/provider/AWS/infra/compute"
-
+  #source = "github.com/arunk4m/vThunder-terraform/tree/master/modules/provider/AWS/infra/compute"
+  source = "../../../../../modules/provider/AWS/infra/compute"
 
 }
 
 module "aws_network" {
 
-  source = "github.com/arunk4m/vThunder-terraform/tree/master/modules/provider/AWS/infra/network"
+  source = "../../../../../modules/provider/AWS/infra/network"
 }
