d5cd3da1d8c842e016578f987b80cb19dd9af8b5
arunk4m <arunk4m@gmail.com>
2019-05-30 14:31:23 +0530

    aws.tf

--- modules/provider/AWS/infra/compute/aws.tf
+++ modules/provider/AWS/infra/compute/aws.tf
@@ -1,17 +1,5 @@
-variable "aws_region" {
-    description = "EC2 Region for the VPC"
-    default = "us-east-2"
-}
-
-variable "my_profile" {
-  default="gslab"
-}
-
-
-
 provider "aws" {
-    #access_key = "${var.aws_access_key}"
-    #secret_key = "${var.aws_secret_key}"
-    aws_region = "${var.aws_region}"
-    #profile= "${var.my_profile}"
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "us-east-2"
 }
